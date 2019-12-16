0f7189a7ddce8408907b6cef17de948f1c5b991b
moliver4 <tifftang22@gmail.com>
2019-12-16 09:47:07 -0800

    Done.

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -1,5 +1,9 @@
 class SightingSerializer
- 
+  
+    def initialize(sighting_object)
+        @sighting = sighting_object
+    end
+    
     def to_serialized_json
         options = {
           include: {
